e7e609e0e65f68e781c7bade339910893ff1a4b7
ShuaiMou <shuaimou77@gmail.com>
2020-02-19 10:46:26 +1100

    列出大致的整合技术

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # spring-boot-module
 to integrate some spring-boot demo
+
+# 整合SpringBoot的各个模块
+
+## [redis]()
+## [ActiveMQ]()
+## [RocketMQMQ]()
+## [MyBatis]()
+## [ElasticSearch]()
+## [LogBack]()
---
+++ studySpringBootRedis/src/main/java/com/unimelb/saul/studySpringBootRedis/StudySpringBootRedisApplication.java
@@ -0,0 +1,13 @@
+package com.unimelb.saul.studySpringBootRedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringBootRedisApplication.class, args);
+	}
+
+}
