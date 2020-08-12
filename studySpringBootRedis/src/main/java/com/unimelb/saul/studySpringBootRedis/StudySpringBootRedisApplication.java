58bb6da7460ff3b715b4a7b8035ff333b707f376
ShuaiMou <shuaimou77@gmail.com>
2020-08-12 13:39:08 +1000

    使用 maven clean 清除 target 文件

--- studySpringBootRedis/src/main/java/com/unimelb/saul/studySpringBootRedis/StudySpringBootRedisApplication.java
+++ studySpringBootRedis/src/main/java/com/unimelb/saul/studySpringBootRedis/StudySpringBootRedisApplication.java
@@ -2,8 +2,10 @@ package com.unimelb.saul.studySpringBootRedis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StudySpringBootRedisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudySpringBootRedisApplication.class, args);
